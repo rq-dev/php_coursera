9b479472903ef6a9bc6514561ec10d055804c70c
Роман Ященко <romkaq@Romans-MacBook-Pro.local>
2020-11-27 00:05:12 +0500

    Initial commit

---
+++ hw1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Roman Yaschenko</title>
+</head>
+<body>
+<h1>Roman Yaschenko PHP</h1>
+<pre>
+    0 0 0 0 0
+    0       0
+    0       0
+    0 0 0 0 0
+    00
+    0  0
+</pre>
+<?php
+  echo hash('sha256', 'Roman Yashcenko');
+?>
+
+
+
+</body>
+</html>
